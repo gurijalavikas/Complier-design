4710beed30ca2c95c091df212af682778d15a8b0
gurijalavikas <124266420+gurijalavikas@users.noreply.github.com>
2024-07-26 13:40:58 +0530

    Add files via upload

--- lex.yy.c
+++ lex.yy.c
@@ -282,11 +282,12 @@ static void yy_fatal_error YY_PROTO(( yyconst char msg[] ));
 	*yy_cp = '\0'; \
 	yy_c_buf_p = yy_cp;
 
-#define YY_NUM_RULES 4
-#define YY_END_OF_BUFFER 5
-static yyconst short int yy_accept[9] =
+#define YY_NUM_RULES 3
+#define YY_END_OF_BUFFER 4
+static yyconst short int yy_accept[16] =
     {   0,
-        0,    0,    5,    4,    3,    1,    2,    0
+        2,    2,    4,    2,    3,    2,    2,    2,    2,    2,
+        2,    2,    1,    1,    0
     } ;
 
 static yyconst int yy_ec[256] =
@@ -295,16 +296,16 @@ static yyconst int yy_ec[256] =
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    3,    4,    4,    4,    3,    4,
-        4,    4,    3,    4,    4,    4,    4,    4,    3,    4,
-        4,    4,    4,    4,    3,    4,    4,    4,    4,    4,
-        1,    1,    1,    1,    1,    1,    3,    4,    4,    4,
+        1,    1,    1,    1,    1,    1,    1,    3,    3,    3,
+        3,    3,    3,    3,    3,    3,    3,    1,    1,    1,
+        1,    1,    1,    4,    3,    3,    3,    3,    3,    3,
+        3,    3,    3,    3,    3,    3,    3,    3,    3,    3,
+        3,    3,    3,    3,    3,    3,    3,    3,    3,    3,
+        1,    1,    1,    1,    3,    1,    5,    5,    5,    5,
 
-        3,    4,    4,    4,    3,    4,    4,    4,    4,    4,
-        3,    4,    4,    4,    4,    4,    3,    4,    4,    4,
-        4,    4,    1,    1,    1,    1,    1,    1,    1,    1,
+        5,    5,    5,    5,    5,    5,    5,    5,    5,    5,
+        5,    5,    5,    5,    5,    5,    5,    5,    5,    5,
+        5,    5,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -321,29 +322,35 @@ static yyconst int yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static yyconst int yy_meta[5] =
+static yyconst int yy_meta[6] =
     {   0,
-        1,    1,    1,    1
+        1,    2,    1,    1,    1
     } ;
 
-static yyconst short int yy_base[9] =
+static yyconst short int yy_base[18] =
     {   0,
-        0,    0,    5,    6,    6,    6,    6,    6
+        0,    0,   16,    0,   17,    5,    0,    0,   10,    9,
+        8,    7,    0,    0,   17,   10,    6
     } ;
 
-static yyconst short int yy_def[9] =
+static yyconst short int yy_def[18] =
     {   0,
-        8,    1,    8,    8,    8,    8,    8,    0
+       15,    1,   15,   16,   15,   15,   16,    6,   16,   17,
+       16,   17,   11,   12,    0,   15,   15
     } ;
 
-static yyconst short int yy_nxt[11] =
+static yyconst short int yy_nxt[23] =
     {   0,
-        4,    5,    6,    7,    8,    3,    8,    8,    8,    8
+        4,    5,    6,    4,    6,    7,   11,    8,    9,    8,
+        7,   14,   13,   12,   10,   15,    3,   15,   15,   15,
+       15,   15
     } ;
 
-static yyconst short int yy_chk[11] =
+static yyconst short int yy_chk[23] =
     {   0,
-        1,    1,    1,    1,    3,    8,    8,    8,    8,    8
+        1,    1,    1,    1,    1,    6,   17,    6,    6,    6,
+       16,   12,   11,   10,    9,    3,   15,   15,   15,   15,
+       15,   15
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -357,12 +364,11 @@ static char *yy_last_accepting_cpos;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "count.l.txt"
+#line 1 "email.l.txt"
 #define INITIAL 0
-#line 2 "count.l.txt"
-#include <stdio.h>
-int vowel_count = 0, consonant_count = 0;
-#line 366 "lex.yy.c"
+#line 2 "email.l.txt"
+#include<stdio.h>
+#line 372 "lex.yy.c"
 
 /* Macros after this point can all be overridden by user definitions in
  * section 1.
@@ -513,9 +519,9 @@ YY_DECL
 	register char *yy_cp, *yy_bp;
 	register int yy_act;
 
-#line 6 "count.l.txt"
+#line 4 "email.l.txt"
 
-#line 519 "lex.yy.c"
+#line 525 "lex.yy.c"
 
 	if ( yy_init )
 		{
@@ -566,13 +572,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 9 )
+				if ( yy_current_state >= 16 )
 					yy_c = yy_meta[(unsigned int) yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 6 );
+		while ( yy_base[yy_current_state] != 17 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -600,25 +606,20 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 7 "count.l.txt"
-{ vowel_count++; }
+#line 5 "email.l.txt"
+{printf("%s is a valid email", yytext);}
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 8 "count.l.txt"
-{ consonant_count++; }
+#line 6 "email.l.txt"
+{printf("It is not a valid email");}
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 9 "count.l.txt"
-{ printf("Vowels: %d, Consonants: %d\n", vowel_count, consonant_count); }
-	YY_BREAK
-case 4:
-YY_RULE_SETUP
-#line 10 "count.l.txt"
+#line 7 "email.l.txt"
 ECHO;
 	YY_BREAK
-#line 622 "lex.yy.c"
+#line 623 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -910,7 +911,7 @@ static yy_state_type yy_get_previous_state()
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 9 )
+			if ( yy_current_state >= 16 )
 				yy_c = yy_meta[(unsigned int) yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
@@ -945,11 +946,11 @@ yy_state_type yy_current_state;
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 9 )
+		if ( yy_current_state >= 16 )
 			yy_c = yy_meta[(unsigned int) yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
-	yy_is_jam = (yy_current_state == 8);
+	yy_is_jam = (yy_current_state == 15);
 
 	return yy_is_jam ? 0 : yy_current_state;
 	}
@@ -1504,11 +1505,12 @@ int main()
 	return 0;
 	}
 #endif
-#line 10 "count.l.txt"
+#line 7 "email.l.txt"
 
-
-int main() {
-  yylex();
+int main()
+{
+printf("\n Enter the email:");	
+yylex();
 }
 int yywrap()
 {
---
+++ sample.c
@@ -0,0 +1,10 @@
+#include<stdio.h> 
+int main()
+{
+int numberl. number2, sum ;
+printf("Enter two integers.");
+scanf("%d %d", &mumber1, &number2);
+sum = numberl - number 2;
+printf("%d + %d =%d", number1, number2, sum):
+return 0;
+}
